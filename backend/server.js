32310b739bcf6bd163a9bba741261d9bd55b9dc7
NaderShreef <nadersgn@gmail.com>
2024-11-28 20:45:36 +0200

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = 5000;
 
 // Routes
-const studentRoutes = require('./routes/studentRoutes');
-const courseRoutes = require('./routes/courseRoutes');
+const studentRoutes = require("./routes/studentRoutes");
+const courseRoutes = require("./routes/courseRoutes");
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/students', studentRoutes);
-app.use('/api/courses', courseRoutes);
+app.use("/api/students", studentRoutes);
+app.use("/api/courses", courseRoutes);
 
 // MongoDB Connection with updated options
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection failed:', err.message));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection failed:", err.message));
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 // Start the Server
